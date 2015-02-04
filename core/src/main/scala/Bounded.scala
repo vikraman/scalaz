84ae8a935706c01cf7dcaa7a0057776179f43075
Alois Cochard <alois.cochard@gmail.com>
2015-02-04 14:20:25 +0100

    Add `Bounded` class.

---
+++ core/src/main/scala/Bounded.scala
@@ -0,0 +1,13 @@
+package scalaz
+
+trait Bounded[A] {
+  def min: A
+  def max: A
+}
+
+object Bounded {
+  def apply[A](min0: A, max0: A): Bounded[A] = new Bounded[A] { val min = min0; val max = max0 }
+  def minBound[A](implicit A: Bounded[A]): A = A.min
+  def maxBound[A](implicit A: Bounded[A]): A = A.max
+}
+
