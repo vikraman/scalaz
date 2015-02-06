73abe098138b9176706cb924a6704f8c358a1562
Alois Cochard <alois.cochard@gmail.com>
2015-02-06 20:56:33 +0100

    Add `Iso` data type.

---
+++ core/src/main/scala/Iso.scala
@@ -0,0 +1,12 @@
+package scalaz
+
+trait Iso[S, T, A, B] {
+  def apply[P[_, _], F[_]](p: P[A, F[B]])(implicit F: Functor[F], P: Profunctor[P]): P[S, F[T]]
+}
+
+object Iso {
+  def apply[S, T, A, B](sa: S => A, bt: B => T): Iso[S, T, A, B] = new Iso[S, T, A, B] {
+    def apply[P[_, _], F[_]](p: P[A, F[B]])(implicit F: Functor[F], P: Profunctor[P]): P[S, F[T]] =
+      P.dimap(sa)(F.map(bt))(p)
+  }
+}
