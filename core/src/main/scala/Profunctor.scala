c650f1a952ca6c186d8c5cb7f6137c51c9c4f634
Alois Cochard <alois.cochard@gmail.com>
2015-02-06 20:56:20 +0100

    Add `Profunctor` class.

---
+++ core/src/main/scala/Profunctor.scala
@@ -0,0 +1,7 @@
+package scalaz
+
+trait Profunctor[F[_, _]] {
+  def lmap[A, B, C](f: A => B): F[B, C] => F[A, C] = dimap[A, B, C, C](f)(identity)
+  def rmap[A, B, C](f: B => C): F[A, B] => F[A, C] = dimap[A, A, B, C](identity)(f)
+  def dimap[A, B, C, D](f: A => B)(g: C => D): F[B, C] => F[A, D] = lmap(f).andThen(rmap(g))
+}
