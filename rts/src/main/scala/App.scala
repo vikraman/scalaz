9cf58a1d47cb7f97c12fa89895ab0ad728a47289
Alois Cochard <alois.cochard@gmail.com>
2015-02-07 14:37:50 +0100

    Add `rts/App`.

--- rts/src/main/scala/App.scala
+++ rts/src/main/scala/App.scala
@@ -1,11 +1,9 @@
+package scalaz
 
-/*
-  trait App {
-    def mainIO: IO[Unit]
-    def rts: RTS = defaultRTS
+trait App {
+  def mainIO: IO[Unit]
+  def rts: RTS = RTS.defaultRTS
 
-    // TODO Feed arguments with the STM in IO? Or find
-    def main(args: Array[String]): Unit = rts.unsafePerformIO_(mainIO)
-  }
-
-  */
+  // TODO Feed arguments with the STM in IO? ... find something neat
+  def main(args: Array[String]): Unit = rts.unsafePerformIO_(mainIO)
+}
