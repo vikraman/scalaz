be7faec284dc07c02001dcb444e44fd49a1009e8
Alois Cochard <alois.cochard@gmail.com>
2015-02-07 13:27:44 +0100

    Rename `Iso` constructor.

--- core/src/main/scala/Iso.scala
+++ core/src/main/scala/Iso.scala
@@ -5,7 +5,7 @@ trait Iso[S, T, A, B] {
 }
 
 object Iso {
-  def apply[S, T, A, B](sa: S => A, bt: B => T): Iso[S, T, A, B] = new Iso[S, T, A, B] {
+  def iso[S, T, A, B](sa: S => A, bt: B => T): Iso[S, T, A, B] = new Iso[S, T, A, B] {
     def apply[P[_, _], F[_]](p: P[A, F[B]])(implicit F: Functor[F], P: Profunctor[P]): P[S, F[T]] =
       P.dimap(sa)(F.map(bt))(p)
   }
