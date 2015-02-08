593f72f812c295690e0fab80be3e39b1b3085574
Alois Cochard <alois.cochard@gmail.com>
2015-02-08 22:19:44 +0100

    Add misc `Maybe` functions.

--- prelude/src/main/scala/Maybe.scala
+++ prelude/src/main/scala/Maybe.scala
@@ -6,6 +6,14 @@ object Maybe {
   final case class Empty[A]() extends Maybe[A]
   final case class Just[A](a: A) extends Maybe[A]
 
+  def empty[A]: Maybe[A] = Empty()
+  def just[A](a: A): Maybe[A] = Just(a)
+
+  def maybe[A, B](n: B)(f: A => B): Maybe[A] => B = _ match {
+    case Empty() => n
+    case Just(x) => f(x)
+  }
+
   def fromOption[A](oa: Option[A]): Maybe[A] = oa.fold[Maybe[A]](Empty[A])(Just(_))
 
   trait Syntax {
