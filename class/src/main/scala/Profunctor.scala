7130be3e148bdf9dcf405abd6d0151791f6016ec
Alois Cochard <alois.cochard@gmail.com>
2015-02-08 18:35:33 +0100

    Add `Profunctor` instances for `->`.

--- class/src/main/scala/Profunctor.scala
+++ class/src/main/scala/Profunctor.scala
@@ -1,16 +1,24 @@
 package scalaz
 
 import Prelude._
+import Either._
+import Either.Syntax._
 
 trait Profunctor[F[_, _]] {
-  def lmap[A, B, C](f: A => B): F[B, C] => F[A, C] = dimap[A, B, C, C](f)(id)
-  def rmap[A, B, C](f: B => C): F[A, B] => F[A, C] = dimap[A, A, B, C](id)(f)
-  def dimap[A, B, C, D](f: A => B)(g: C => D): F[B, C] => F[A, D] = lmap(f).andThen(rmap(g))
+  def lmap[A, B, C](ab: A => B): F[B, C] => F[A, C] = dimap[A, B, C, C](ab)(id)
+  def rmap[A, B, C](bc: B => C): F[A, B] => F[A, C] = dimap[A, A, B, C](id)(bc)
+  def dimap[A, B, C, D](ab: A => B)(bc: C => D): F[B, C] => F[A, D] = lmap(ab).andThen(rmap(bc))
 }
 
 object Profunctor {
+  implicit val fun: Profunctor[->] = new Profunctor[->] {
+    override def lmap[A, B, C](ab: A => B): (B => C) => (A => C) = bc => a => bc(ab(a))
+    override def rmap[A, B, C](bc: B => C): (A => B) => (A => C) = ab => a => bc(ab(a))
+    override def dimap[A, B, C, D](ab: A => B)(cd: C => D): (B => C) => (A => D) = bc => a => cd(bc(ab(a)))
+  }
+
   trait Strong[P[_, _]] {
-    val profunctor: Profunctor[P]
+    def profunctor: Profunctor[P]
 
     def first[A, B, C](pab: P[A, B]): P[(A, C), (B, C)] =
       profunctor.dimap[(A, C), (C, A), (C, B), (B, C)](_.swap)(_.swap)(second(pab))
@@ -22,13 +30,21 @@ object Profunctor {
   trait Choice[P[_, _]] {
     import Either._
 
-    val profunctor: Profunctor[P]
+    def profunctor: Profunctor[P]
 
     def left[A, B, C](pab: P[A, B]): P[A \/ C, B \/ C] =
       profunctor.dimap[A \/ C, C \/ A, C \/ B, B \/ C](swap(_))(swap(_))(right(pab))
 
     def right[A, B, C](pab: P[A, B]): P[C \/ A, C \/ B] =
       profunctor.dimap[C \/ A, A \/ C, B \/ C, C \/ B](swap(_))(swap(_))(left(pab))
+  }
+
+  object Choice {
+    implicit val fun: Choice[->] = new Choice[->] {
+      val profunctor = Profunctor.fun
+      override def left[A, B, C](ab: A => B): A \/ C => B \/ C  = _.fold[B \/ C](a => Left(ab(a)))(Right(_))
+      override def right[A, B, C](ab: A => B): C \/ A => C \/ B = _.fold[C \/ B](Left(_))((a => Right(ab(a))))
+    }
   }
 
   final case class UpStar[F[_], A, B](run: A => F[B])
