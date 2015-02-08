08458da9d25827e052db1dc9c2141cdf4fa651e2
Alois Cochard <alois.cochard@gmail.com>
2015-02-08 15:04:32 +0100

    Add `Either.either` and syntax equivalent.

--- prelude/src/main/scala/Either.scala
+++ prelude/src/main/scala/Either.scala
@@ -8,12 +8,20 @@ object Either {
   final case class Left[A](a: A) extends (A \/ Nothing)
   final case class Right[A](a: A) extends (Nothing \/ A)
 
-  def fromScala[A, B](ab: scala.Either[A, B]): Either[A, B] = ab.fold(Left(_), Right(_))
+  def either[A, B, C](ac: A => C)(bc: B => C): Either[A, B] => C = _ match {
+    case Left(l)  => ac(l)
+    case Right(r) => bc(r)
+  }
 
   def swap[A, B](ab: Either[A, B]): Either[B, A] = ???
 
+  def fromScala[A, B](ab: scala.Either[A, B]): Either[A, B] = ab.fold(Left(_), Right(_))
+
   trait Syntax {
-    implicit class EitherOps[A](a: A) {
+    implicit class EitherOps[A, B](ab: Either[A, B]) {
+      def fold[C](l: A => C, r: B => C): C = either(l)(r)(ab)
+    }
+    implicit class ToEitherOps[A](a: A) {
       def left[B]: Either[A, B] = Left(a)
       def right[B]: Either[B, A] = Right(a)
     }
