fa1a0e5568eb8ff856eef0c7c05aeca387ec9bcf
Alois Cochard <alois.cochard@gmail.com>
2015-02-09 00:04:24 +0100

    Extract `Compose` into `Optic`.

--- core/src/main/scala/Lens.scala
+++ core/src/main/scala/Lens.scala
@@ -3,7 +3,7 @@ package scalaz
 abstract class Lens[S, T, A, B] {
   def apply[F[_]](f: A => F[B])(implicit F: Functor[F]): S => F[T]
 
-  def ∘[I, O](i: I)(implicit Compose: Lens.Compose[I, O, S, T, A, B]): O = Compose(this)(i)
+  def ∘[U, V](u: U)(implicit Compose: Optic.Compose[Lens, U, V, S, T, A, B]): V = Compose(this)(u)
 }
 
 object Lens {
@@ -14,28 +14,4 @@ object Lens {
   }
 
   def slens[S, A](sa: S => A)(sas: S => A => S): Lens[S, S, A, A] = lens[S, S, A, A](sa)(sas)
-
-  trait Compose[I, O, S, T, A, B] { def apply(lens: Lens[S, T, A, B]): I => O }
-
-  object Compose {
-    implicit def lens[S, T, A, B, C, D]: Compose[Lens[A, B, C, D], Lens[S, T, C, D], S, T, A, B] =
-      new Compose[Lens[A, B, C, D], Lens[S, T, C, D], S, T, A, B] {
-        def apply(stab: Lens[S, T, A, B]): Lens[A, B, C, D] => Lens[S, T, C, D] = abcd => new Lens[S, T, C, D] {
-          def apply[F[_]](afb: C => F[D])(implicit F: Functor[F]): S => F[T] = stab.apply(abcd(afb))
-        }
-      }
-
-    implicit def traversal[S, T, A, B, C, D]: Compose[Traversal[A, B, C, D], Traversal[S, T, C, D], S, T, A, B] =
-      new Compose[Traversal[A, B, C, D], Traversal[S, T, C, D], S, T, A, B] {
-        def apply(stab: Lens[S, T, A, B]): Traversal[A, B, C, D] => Traversal[S, T, C, D] = abcd => new Traversal[S, T, C, D] {
-          def apply[F[_]](afb: C => F[D])(implicit F: Applicative[F]): S => F[T] = stab.apply(abcd(afb))(F.apply.functor)
-        }
-      }
-
-    implicit def prism[S, T, A, B, C, D]: Compose[Prism[A, B, C, D], Traversal[S, T, C, D], S, T, A, B] =
-      new Compose[Prism[A, B, C, D], Traversal[S, T, C, D], S, T, A, B] {
-        def apply(stab: Lens[S, T, A, B]): Prism[A, B, C, D] => Traversal[S, T, C, D] = abcd =>
-          traversal[S, T, A, B, C, D](stab)(abcd.asTraversal)
-      }
-  }
 }
--- core/src/main/scala/Optic.scala
+++ core/src/main/scala/Optic.scala
@@ -11,4 +11,44 @@ object Optic {
   }
 
   object Types extends Types
+
+  abstract class Compose[O[_, _, _, _], U, V, S, T, A, B] { def apply(lens: O[S, T, A, B]): U => V }
+
+  type Compose_[O[_, _, _, _], P[_, _, _, _], Q[_, _, _, _], S, T, A, B, C, D] =
+    Compose[O, P[A, B, C, D], Q[S, T, C, D], S, T, A, B]
+
+  object Compose {
+    implicit def lens_lens[S, T, A, B, C, D]: Compose_[Lens, Lens, Lens, S, T, A, B, C, D] =
+      new Compose[Lens, Lens[A, B, C, D], Lens[S, T, C, D], S, T, A, B] {
+        def apply(stab: Lens[S, T, A, B]): Lens[A, B, C, D] => Lens[S, T, C, D] = abcd => new Lens[S, T, C, D] {
+          def apply[F[_]](afb: C => F[D])(implicit F: Functor[F]): S => F[T] = stab.apply(abcd(afb))
+        }
+      }
+
+    implicit def lens_traversal[S, T, A, B, C, D]: Compose_[Lens, Traversal, Traversal, S, T, A, B, C, D] =
+      new Compose[Lens, Traversal[A, B, C, D], Traversal[S, T, C, D], S, T, A, B] {
+        def apply(stab: Lens[S, T, A, B]): Traversal[A, B, C, D] => Traversal[S, T, C, D] = abcd => new Traversal[S, T, C, D] {
+          def apply[F[_]](afb: C => F[D])(implicit F: Applicative[F]): S => F[T] = stab.apply(abcd(afb))(F.apply.functor)
+        }
+      }
+
+    implicit def lens_prism[S, T, A, B, C, D]: Compose_[Lens, Prism, Traversal, S, T, A, B, C, D] =
+      new Compose[Lens, Prism[A, B, C, D], Traversal[S, T, C, D], S, T, A, B] {
+        def apply(stab: Lens[S, T, A, B]): Prism[A, B, C, D] => Traversal[S, T, C, D] = abcd =>
+          lens_traversal[S, T, A, B, C, D](stab)(abcd.asTraversal)
+      }
+
+    implicit def traversal_lens[S, T, A, B, C, D]: Compose_[Traversal, Lens, Traversal, S, T, A, B, C, D] =
+      new Compose[Traversal, Lens[A, B, C, D], Traversal[S, T, C, D], S, T, A, B] {
+        def apply(stab: Traversal[S, T, A, B]): Lens[A, B, C, D] => Traversal[S, T, C, D] = abcd => new Traversal[S, T, C, D] {
+          def apply[F[_]](afb: C => F[D])(implicit F: Applicative[F]): S => F[T] = stab.apply(abcd(afb)(F.apply.functor))
+        }
+      }
+    implicit def traversal_traversal[S, T, A, B, C, D]: Compose_[Traversal, Traversal, Traversal, S, T, A, B, C, D] =
+      new Compose[Traversal, Traversal[A, B, C, D], Traversal[S, T, C, D], S, T, A, B] {
+        def apply(stab: Traversal[S, T, A, B]): Traversal[A, B, C, D] => Traversal[S, T, C, D] = abcd => new Traversal[S, T, C, D] {
+          def apply[F[_]](afb: C => F[D])(implicit F: Applicative[F]): S => F[T] = stab.apply(abcd(afb))
+        }
+      }
+  }
 }
