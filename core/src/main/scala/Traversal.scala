445a20ead9bfecd3ccd43909862b9ca5f531ebcb
Alois Cochard <alois.cochard@gmail.com>
2015-02-09 09:58:31 +0100

    Revert "Extract `Compose` into `Optic`."
    This reverts commit fa1a0e5568eb8ff856eef0c7c05aeca387ec9bcf.
    
    Conflicts:
    	core/src/main/scala/Lens.scala
    	core/src/main/scala/Optic.scala
    	core/src/main/scala/Traversal.scala
    
    It seems better to keep then seperated for maintainability and might be
    better in term of implicit resolution load.

--- core/src/main/scala/Traversal.scala
+++ core/src/main/scala/Traversal.scala
@@ -3,7 +3,7 @@ package scalaz
 abstract class Traversal[S, T, A, B] {
   def apply[F[_]](afb: A => F[B])(implicit F: Applicative[F]): S => F[T]
 
-  def ∘[U, V](u: U)(implicit Compose: Optic.Compose[Traversal, U, V, S, T, A, B]): V = Compose(this)(u)
+  def ∘[I, O](i: I)(implicit Compose: Traversal.Compose[I, O, S, T, A, B]): O = Compose(this)(i)
 }
 
 object Traversal {
@@ -13,6 +13,23 @@ object Traversal {
   object Types extends Types
 
   trait Functions {
-    def _head[F[_], A]: Traversal_[F[A], A] =
+    //def _head[F[_], A]: Traversal_[F[A], A] =
+  }
+
+  trait Compose[I, O, S, T, A, B] { def apply(lens: Traversal[S, T, A, B]): I => O }
+
+  object Compose {
+    implicit def lens[S, T, A, B, C, D]: Compose[Lens[A, B, C, D], Traversal[S, T, C, D], S, T, A, B] =
+      new Compose[Lens[A, B, C, D], Traversal[S, T, C, D], S, T, A, B] {
+        def apply(stab: Traversal[S, T, A, B]): Lens[A, B, C, D] => Traversal[S, T, C, D] = abcd => new Traversal[S, T, C, D] {
+          def apply[F[_]](afb: C => F[D])(implicit F: Applicative[F]): S => F[T] = stab.apply(abcd(afb)(F.apply.functor))
+        }
+      }
+    implicit def traversal[S, T, A, B, C, D]: Compose[Traversal[A, B, C, D], Traversal[S, T, C, D], S, T, A, B] =
+      new Compose[Traversal[A, B, C, D], Traversal[S, T, C, D], S, T, A, B] {
+        def apply(stab: Traversal[S, T, A, B]): Traversal[A, B, C, D] => Traversal[S, T, C, D] = abcd => new Traversal[S, T, C, D] {
+          def apply[F[_]](afb: C => F[D])(implicit F: Applicative[F]): S => F[T] = stab.apply(abcd(afb))
+        }
+      }
   }
 }
