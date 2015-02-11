d028d05662ccdf1e248603f017ba9cfdb60aa088
Alois Cochard <alois.cochard@gmail.com>
2015-02-11 23:23:17 +0100

    Add `z` annotation to write method implicit context.
    Z stand for Zorro.

---
+++ meta/src/main/scala/z.scala
@@ -0,0 +1,56 @@
+package scalaz
+
+import scala.language.experimental.macros
+import scala.reflect.macros.blackbox.Context
+
+class z extends scala.annotation.StaticAnnotation {
+  def macroTransform(annottees: Any*): Any = macro z.apply
+}
+
+object z {
+  def apply(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
+    import c.universe._
+    import c.universe.Flag._
+
+    import scala.Predef.println
+
+    val transform: DefDef => DefDef = _ match {
+      case DefDef(mods, name, tparams, vparamss, tpt, rhs) =>
+        val vals = vparamss.flatMap(id).filter(_.mods.hasFlag(IMPLICIT))
+
+        val implicits: List[Tree] = vals.flatMap { v =>
+          val tree = v.tpt
+          val AppliedTypeTree(ident, _) = tree
+          val tpe = c.typecheck(ident, c.TYPEmode).tpe
+          tpe.decls.collect { case m: MethodSymbol if m.isImplicit =>
+            val n = s"${v.name}_${m.name}"
+            val i = Select(Ident(v.name), m.name)
+            ValDef(Modifiers(IMPLICIT), TermName(n), TypeTree(), i)
+          }
+        }
+
+        DefDef(mods, name, tparams, vparamss, tpt, q"{..${implicits :+ rhs}}")
+    }
+
+    val transformTmp: Template => Template = _ match {
+      case Template(parents, self, body) => Template(parents, self, body.collect {
+        case d: DefDef => transform(d)
+        case t => t
+      })
+    }
+
+    val trees = annottees.map( _.tree match {
+      case ClassDef(mods, name, tparams, tmp) =>
+        ClassDef(mods, name, tparams, transformTmp(tmp))
+      case ModuleDef(mods, name, tmp) =>
+        ModuleDef(mods, name, transformTmp(tmp))
+      case d: DefDef => transform(d)
+      case tree =>
+        println(showRaw(tree))
+        c.warning(c.enclosingPosition, "The annotation have no effect here.")
+        tree
+    })
+
+    c.Expr[Any](q"{ ${trees:_*} }")
+  }
+}
