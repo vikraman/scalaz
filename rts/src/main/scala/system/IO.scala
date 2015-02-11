3dc58b5e072503875cc66f4ab963bd9d79a2995b
Alois Cochard <alois.cochard@gmail.com>
2015-02-11 12:30:49 +0100

    Add `system.IO.print`.

--- rts/src/main/scala/system/IO.scala
+++ rts/src/main/scala/system/IO.scala
@@ -7,5 +7,7 @@ package object IO {
   val getLine = captureIO(scala.io.StdIn.readLine)
   val putStrLn = liftIO(scala.Predef.println _)
   val threadDelay = liftIO(Thread.sleep _)
+
+  def print[A](a: A)(implicit A: Show[A]): IO[Unit] = putStrLn(A.show(a))
 }
 
