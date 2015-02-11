c3cffcf222e7bf95adf52acd2ece18f329798d58
Alois Cochard <alois.cochard@gmail.com>
2015-02-11 23:34:10 +0100

    Add expanded code in the example of the z annotation.

--- examples/src/main/scala/z.scala
+++ examples/src/main/scala/z.scala
@@ -7,4 +7,20 @@ import scalaz._
   def bar[F[_]: Functor, A](xs: F[A]): F[A] = xs.map(id)
   def foo[M[_]: Monad, N[_]: Monad, A](xs: M[A], ys: N[A]): (M[A], N[A]) =
     (bar(xs), bar(ys))
+
+  /*
+  def foo[M[_]: Monad, N[_]: Monad, A](xs: M[A], ys: N[A]): (M[A], N[A]) = {
+    implicit def ev$1_bind = ev$1.bind
+    implicit def ev$1_applicative = ev$1_applicative
+    implicit def ev$1_apply = ev$1.apply
+    implicit def ev$1_functor = ev$1.functor
+
+    implicit def ev$2_bind = ev$2.bind
+    implicit def ev$2_applicative = ev$2_applicative
+    implicit def ev$2_apply = ev$2.apply
+    implicit def ev$2_functor = ev$2.functor
+
+    (bar(xs), bar(ys))
+  }
+  */
 }
